6a01aa69d830506d5f64a40b51bfc1ac4c567422
EwaJakub <ewa.jak@onet.pl>
2023-04-19 20:34:23 +0200

    Wyrazenia_regularne - exercise_2

--- 01_Dzien_01/04_Wyrazenie_regularne/02_Zadanie_2/exercise_2.py
+++ 01_Dzien_01/04_Wyrazenie_regularne/02_Zadanie_2/exercise_2.py
@@ -1,6 +1,8 @@
 import re
 
-
 with open('text.txt', 'r') as fin:
     text_to_search = fin.read()
-    #  Tutaj umieść swoje rozwiązanie!
+    print(re.findall(r"autor", text_to_search))
+    print(re.findall(r"\d+%", text_to_search))
+    print(re.findall(r"\w+\.", text_to_search))
+    print(re.findall(r"polski", text_to_search, re.I))
