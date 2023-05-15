24fc00bb6db76b2ef6742dbb9b710fc8c47874dc
EwaJakub <ewa.jak@onet.pl>
2023-05-15 21:26:45 +0200

    Praca_domowa - Price

--- 04_Dzien_2_-_praca_domowa/02_Price/test_exercise.py
+++ 04_Dzien_2_-_praca_domowa/02_Price/test_exercise.py
@@ -5,31 +5,31 @@ import exercise
 
 class Price23VatTestCase(unittest.TestCase):
     def test_creating_instance(self):
-        p = exercise.Price(123)
+        p = exercise.Price23Vat(123)
 
     def test_reading(self):
-        p = exercise.Price(123)
-        self.assertAlmostEqual(p.get_netto(), 100)
-        self.assertEqual(p.get_brutto(), 123)
+        p = exercise.Price23Vat(123)
+        self.assertAlmostEqual(p.get_net(), 100)
+        self.assertEqual(p.get_pretax(), 123)
         self.assertAlmostEqual(p.get_tax(), 23)
 
     def test_setting_netto(self):
-        p = exercise.Price(123)
-        p.set_netto(200)
-        self.assertEqual(p.get_netto(), 200)
-        self.assertAlmostEqual(p.get_brutto(), 246)
+        p = exercise.Price23Vat(123)
+        p.set_net(200)
+        self.assertEqual(p.get_net(), 200)
+        self.assertAlmostEqual(p.get_pretax(), 246)
         self.assertAlmostEqual(p.get_tax(), 46)
 
     def test_setting_brutto(self):
-        p = exercise.Price(123)
-        p.set_brutto(246)
-        self.assertAlmostEqual(p.get_netto(), 200)
-        self.assertEqual(p.get_brutto(), 246)
+        p = exercise.Price23Vat(123)
+        p.set_pretax(246)
+        self.assertAlmostEqual(p.get_net(), 200)
+        self.assertEqual(p.get_pretax(), 246)
         self.assertAlmostEqual(p.get_tax(), 46)
 
     def test_setting_tax(self):
-        p = exercise.Price(123)
+        p = exercise.Price23Vat(123)
         p.set_tax(46)
-        self.assertAlmostEqual(p.get_netto(), 200)
-        self.assertAlmostEqual(p.get_brutto(), 246)
+        self.assertAlmostEqual(p.get_net(), 200)
+        self.assertAlmostEqual(p.get_pretax(), 246)
         self.assertEqual(p.get_tax(), 46)
